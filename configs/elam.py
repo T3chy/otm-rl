73a917965c75564ca53a9ed395ededb3c869467e
Elam Day <elam.day.friedland@gmail.com>
2020-12-01 18:19:45 -0800

    one working now

---
+++ configs/elam.py
@@ -0,0 +1,29 @@
+
+from OTMWrapper import OTMWrapper
+
+start_time = 0.
+duration = 3600.
+advance_time = 300.
+
+# load the configuration file
+name = '50nodes.xml'
+otm = OTMWrapper(name)
+
+# initialize (prepare/rewind the simulation)
+otm.initialize(1.)
+
+# run step-by-step using the 'advance' method
+time = start_time
+end_time = start_time + duration
+
+while(time<end_time):
+  otm.advance(advance_time)
+
+  # Insert your code here -----
+  print(otm.otm.get_current_time())
+  time += advance_time;
+
+# always end by deleting the wrapper
+print(otm.get_links_table())
+print(otm.to_networkx())
+del otm
